9e90303ac7c8b83520419947153a690802691c81
nrenzoni <nrenzoni1@gmail.com>
2020-01-28 15:39:05 +0000

    finished strategy impl

--- trade_strategy_tester_framework.R
+++ trade_strategy_tester_framework.R
@@ -3,25 +3,22 @@ library(TTR)
 library(R6)
 library(PerformanceAnalytics)
 
-# strategy_func is a function that receives data as xts object
-# and returns xts object of signals
-calculate_strategy <- function(strategy_func, ticker_data) {
-  signals <- strategy_func(ticker_data)
-  return(signals)
-}
+# begin region: helper class and function definitions
 
 StrategySummarizer <- R6Class(
   "StrategySummarizer",
   c(
+    strategy_name = "character",
     tickerData = "xts",
     signals = "xts",
     returns = "xts",
     trade_returns = "xts",
-    
-    initialize = function(tickerData, signals) {
+
+    initialize = function(strategy_name, tickerData, signals) {
+      self$strategy_name <- strategy_name
       self$tickerData <- tickerData
       self$signals <- signals
-      
+
       self$returns <-
         Delt(self$tickerData)
       self$returns[1] <- 0
@@ -30,6 +27,7 @@ StrategySummarizer <- R6Class(
     },
     get_headers = function() {
       headers <- c(
+        "Name",
         "Annual Return",
         "Max Drawdown",
         "Annual Std Deviation",
@@ -46,16 +44,20 @@ StrategySummarizer <- R6Class(
       annual_std_dev <- StdDev.annualized(self$trade_returns)
       val_at_risk <- VaR(self$trade_returns)
       annual_sharpe <- SharpeRatio.annualized(self$trade_returns)
-      
-      data <- c(annual_returns,
-                max_drawdown,
-                annual_std_dev,
-                val_at_risk,
-                annual_sharpe)
-      
+
+      data <- c(
+        self$strategy_name,
+        annual_returns,
+        max_drawdown,
+        annual_std_dev,
+        val_at_risk,
+        annual_sharpe
+      )
+
       return(data)
     },
     plot = function() {
+      names(self$trade_returns) <- self$strategy_name
       charts.PerformanceSummary(self$trade_returns)
     }
   )
@@ -72,121 +74,60 @@ MultipleStrategyCSVSummarizer <- R6Class(
     },
     summarize = function() {
       output <- toString(self$headers)
-      
+
       for (strategy in self$strategies) {
+        # only round the input if it's a number
+        rounded_numbers <- mapply(function(i) {
+          as_number <- as.numeric(i)
+          if (is.na(as_number))
+            return(i)
+          else
+            return(round(as_number, 4))
+        },
+                                  strategy$get_summarized_data())
+
         output <- paste(output,
                         "\n",
-                        toString(strategy$get_summarized_data()),
+                        toString(rounded_numbers),
                         sep = "")
       }
-      
+
       return(output)
     }
   )
 )
-
-# based off https://www.r-bloggers.com/volume-by-price-charts-using-r/
-volume_by_price <- function(ticker_data) {
-  #Add Positive and Negative Volumes
-  ticker_data$positive_VbP <-
-    Vo(ticker_data[which(Lag(Cl(ticker_data)) <=
-                           Cl(ticker_data))])
-  
-  ticker_data$negative_vbP <-
-    Vo(ticker_data[which(Lag(Cl(ticker_data)) >
-                           Cl(ticker_data))])
-  
-  # Since NAs got generated, replace NAs with 0
-  ticker_data[is.na(ticker_data)] <- 0
-  
-  # Divisor for security
-  div <- 1
-  
-  #Identify High and low of Series, as mutiple of div
-  high <- as.integer(ceiling(max(Cl(ticker_data)) / div) * div)
-  low <- as.integer(floor(min(Cl(ticker_data)) / div) * div)
-  
-  # Breaks of interval divisor
-  breaks <- as.integer(seq(low, high, by = div))
-  
-  # Identify and assign price intervals
-  ticker_data$t <-
-    breaks[findInterval(ticker_data$close, breaks, all.inside = T)]
-  
-  # Add Positive / Negative Volumes to block
-  volsP <- unlist(lapply(breaks, function(x) {
-    sum(ticker_data$positive_VbP[ticker_data$t == x])
-  }))
-  volsN <- unlist(lapply(breaks, function(x) {
-    sum(ticker_data$negative_vbP[ticker_data$t == x])
-  }))
-  
-  #Bind the Positive and Negative Volumes
-  vols <- rbind(volsP, volsN)
-  colnames(vols) <- breaks
-  
-  return(vols)
-}
-
-momentum_strategy_1 <- function(ticker_data) {
-  macd <- MACD(
-    ticker_data,
-    nFast = 12,
-    nSlow = 26,
-    nSig = 9,
-    maType = "SMA",
-    percent = F
-  )
-  
-  bb <-
-    BBands(ticker_data,
-           n = 20,
-           maType = "SMA",
-           sd = 2)
-  
-  signal <-
-    ifelse(
-      ticker_data > bb$up &
-        macd$macd > macd$signal,
-      1,
-      ifelse(ticker_data < bb$dn &
-               macd$macd < macd$signal, -1,
-             0)
-    )
-  
-  signal <- na.omit(signal)
-  
-  return(signal)
-}
 
 getTicker <- function(symbol, start, end) {
   data <-
     getSymbols(symbol,
                src = "yahoo",
                auto.assign = F)
-  
+
   data <- window(data,
                  start = start,
                  end = end)
-  
+
   # forward fill NA
   data <- na.locf(data)
-  
+
   names(data) <-
     c("open", "high", "low", "close", "volume", "adjusted")
-  
+
   return(data)
 }
 
+# end region: helper class and function definitions
+
+# begin region: strategy function definition
+
 volume_price_trend_indicator <- function(ticker_data) {
-  
-  ticker_data$price_increase <- rep(NA, times=nrow(ticker_data))
-  
+  ticker_data$price_increase <- rep(NA, times = nrow(ticker_data))
+
   for (i in 2:nrow(ticker_data)) {
-    previous_data <- as.numeric( Cl(ticker_data)[i-1] )
-    current_data <- as.numeric( Cl(ticker_data)[i] )
-    
-    if (previous_data <  current_data) {
+    previous_data <- as.numeric(Cl(ticker_data)[i - 1])
+    current_data <- as.numeric(Cl(ticker_data)[i])
+
+    if (previous_data < current_data) {
       ticker_data$price_increase[i] <- 1
     } else if (previous_data > current_data) {
       ticker_data$price_increase[i] <- -1
@@ -194,57 +135,57 @@ volume_price_trend_indicator <- function(ticker_data) {
       ticker_data$price_increase[i] <- 0
     }
   }
-  
-  ticker_data$volume_price_trend <- rep(NA, times=nrow(ticker_data))
+
+  ticker_data$volume_price_trend <-
+    rep(NA, times = nrow(ticker_data))
   ticker_data$volume_price_trend[1] <- 1
-  
+
   for (i in 2:nrow(ticker_data)) {
-    previous_data <- ticker_data[i-1]
+    previous_data <- ticker_data[i - 1]
     current_data <- ticker_data[i]
-    
+
     previous_close <- as.numeric(Cl(previous_data))
     current_close <- as.numeric(Cl(current_data))
-    
+
     if (abs(as.numeric(current_data$price_increase)) == 1) {
-      percent_close_change <- (current_close - previous_close) / previous_close
-      ticker_data$volume_price_trend[i] <- 
-        percent_close_change * 
-        previous_close +
-        previous_data$volume_price_trend
+      percent_close_change <-
+        (current_close - previous_close) / previous_close
+      ticker_data$volume_price_trend[i] <-
+        percent_close_change *
+          previous_close +
+          previous_data$volume_price_trend
     } else {
-      ticker_data$volume_price_trend[i] <- previous_data$volume_price_trend
+      ticker_data$volume_price_trend[i] <-
+        previous_data$volume_price_trend
     }
   }
-  
+
   return(ticker_data$volume_price_trend)
+}
+
+get_slope_of_points_new <- function(points) {
+  y_first <- as.numeric(points[1])
+  y_last <- as.numeric(points[length(points)])
+  y <- c(y_first, y_last)
+  x <- 1:2
+
+  reg <- lm(y ~ x)
+
+  slope <- reg$coefficients[2]
+
+  return(slope)
 }
 
 get_slope_of_points <- function(points) {
   y <- as.numeric(points)
   x <- 1:length(y)
-  
-  reg <- lm( y ~ x )
-  
+
+  reg <- lm(y ~ x)
+
   slope <- reg$coefficients[2]
-  
+
   return(slope)
 }
-
-generate_signal_on_ma_crossover <-
-  function(in_data, ma_slow_period, ma_fast_period) {
-    ma_slow <- rollmean(in_data, ma_slow_period)
-    ma_fast <- rollmean(in_data, ma_fast_period)
-    
-    in_data$signals <- ifelse(
-      ma_fast > ma_slow &
-        Lag(ma_fast) <= Lag(ma_slow),
-      1,
-      ifelse(ma_fast < ma_slow &
-               Lag(ma_fast) >= Lag(ma_slow), -1 , 0)
-    )
-    
-    return(in_data$signals)
-  }
 
 adx_above_n_and_positive_slope <- function(in_data, n) {
   adx_all <- ADX(in_data)$ADX
@@ -259,23 +200,27 @@ adx_above_n_and_positive_slope <- function(in_data, n) {
 # n3 = slope_threshold
 # sma_range should be < rolling_days_range
 # slope_threshold should be > 0
-volume_trend_vpt <- function(ticker_data, sma_range, rolling_days_range, slope_threshold) {
-  # slope of sma(sma_range) of volume_price_indicator 
-  # of last rolling_days_range >= slope_threshold
+volume_trend_vpt <-
+  function(ticker_data,
+           sma_range,
+           rolling_days_range,
+           slope_threshold) {
+    # slope of sma(sma_range) of volume_price_indicator
+    # of last rolling_days_range >= slope_threshold
 
-  signals <- rep(NA, nrow(ticker_data))
-  
-  for (i in rolling_days_range:nrow(ticker_data)) {
-    current_data <- ticker_data[(i-rolling_days_range+1):i]
-    vpt_indicator <- volume_price_trend_indicator(current_data)
-    sma <- rollmean(vpt_indicator, sma_range)
-    slope <- get_slope_of_points(sma)
-    
-    signals[i] <- slope >= slope_threshold
+    signals <- rep(NA, nrow(ticker_data))
+
+    for (i in rolling_days_range:nrow(ticker_data)) {
+      current_data <- ticker_data[(i - rolling_days_range + 1):i]
+      vpt_indicator <- volume_price_trend_indicator(current_data)
+      sma <- rollmean(vpt_indicator, sma_range)
+      slope <- get_slope_of_points(sma)
+
+      signals[i] <- slope >= slope_threshold
+    }
+
+    return(signals)
   }
-  
-  return(signals)
-}
 
 get_slope_of_sma_n <- function(ticker_data, n) {
   sma <- rollmean(ticker_data, n)
@@ -286,90 +231,73 @@ get_slope_of_sma_n <- function(ticker_data, n) {
 volume_trend_strategy_1 <-
   function(ticker_data, n1, n2, n3, n4, n5) {
     vpt_booleans <- volume_trend_vpt(ticker_data, n1, n2, n3)
-    
+
     # default signal is 0, i.e. do not buy/sell
     ticker_data$signals <- rep(0, nrow(ticker_data))
-    
+
     for (i in n2:nrow(ticker_data)) {
       if (vpt_booleans[i]) {
         current_data <- ticker_data[(i - n2 + 1):i]
-        
+
         # only if both signals pass, perform buy / sell
         if (!adx_above_n_and_positive_slope(current_data, n4)) {
           next
         }
-        
+
         slope <- get_slope_of_sma_n(current_data, n5)
-        
+
         # if slope is positive, buy, else sell
         ticker_data$signals[i] <- ifelse(slope >= 0, 1, -1)
       }
     }
-    
+
     return(ticker_data$signals)
   }
 
-spy <- getTicker("SPY", "2010-01-01", "2019-12-31")
+# end region: strategy function definition
 
-signals <- volume_trend_strategy_1(spy, 10, 50, .1, 35, 5)
+# begin region: execution of strategy
 
-strat1 <- StrategySummarizer$new(Cl(spy), signals)
-strat1$get_headers()
-strat1$get_summarized_data()
+start_date <- "2015-01-01"
+end_date <- "2019-12-31"
 
-start_date <- index(spy)[1]+100
-spy_window <- spy[start_date <= index(spy) &
-                    index(spy) < (start_date + 50)]
-vbp <-
-  volume_by_price(spy_window)
+strategy <- function(ticker_data) {
+  return(volume_trend_strategy_1(ticker_data, 10, 50, .1, 35, 5))
+}
 
-ticker_data <- Cl(spy_window)
+spy <- getTicker("SPY", start_date, end_date)
+spy_signals <- strategy(spy)
+spy_strategy <- StrategySummarizer$new("volume & trend on SPY",
+                                       Cl(spy),
+                                       spy_signals)
 
-# start vbp plot function
-plot(ticker_data,
-     yaxt="n", 
-     ylab="",
-     xlab="Time",
-     ylim=c(min(as.integer(colnames(vbp))),
-            max(as.integer(colnames(vbp)))),
-     main=paste("Close: Volume by Price"),
-     sub="Market Analyzer http://mypapertrades.blogspot.com/")
+aapl <- getTicker("AAPL", start_date, end_date)
+aapl_signals <- strategy(aapl)
+aapl_strategy <- StrategySummarizer$new("volume & trend on AAPL",
+                                        Cl(aapl),
+                                        aapl_signals)
 
-par(new=T)
+# Kohl's Corporation
+kss <- getTicker("KSS", start_date, end_date)
+kss_signals <- strategy(kss)
+kss_strategy <- StrategySummarizer$new("volume & trend on KSS",
+                                       Cl(kss),
+                                       kss_signals)
 
-barplot(height=vbp, 
-        beside=F,
-        horiz=T, 
-        col=c(rgb(0,1,0,alpha=.3),
-              rgb(1,0,0,alpha=.3)),
-        xlim=c(0,max(vbp[1,]+vbp[2,])*1.1), 
-        space=10, 
-        width=3,
-        xaxt="n",
-        yaxt="n",
-        las=2)
+strategies <- c(spy_strategy,
+                aapl_strategy,
+                kss_strategy)
 
-par(new=F)
-# end plot vbp function
+headers <- aapl_strategy$get_headers()
 
-spy_in_sample <- window(spy,
-                        start = "2010-01-01",
-                        end = "2017-12-31")
+all_strategies <- MultipleStrategyCSVSummarizer$new(strategies, headers)
 
-spy_out_sample <- window(spy,
-                         start = "2018-01-01",
-                         end = "2019-12-31")
+all_strategies$summarize()
 
-spy_in_sample$signals <- momentum_strategy_1(spy_in_sample$close)
-spy_out_sample$signals <- momentum_strategy_1(spy_out_sample$close)
+# plot the strategy performance charts
+for (strategy in strategies) {
+  strategy$plot()
+}
 
-strat_in_sample <-
-  StrategySummarizer$new(spy$close, spy_in_sample$signals)
-strat_out_sample <-
-  StrategySummarizer$new(spy$close, spy_out_sample$signals)
 
-multiStratCsvSummarizer <-
-  MultipleStrategyCSVSummarizer$new(c(strat_in_sample, strat_out_sample),
-                                    strat_in_sample$get_headers())
-
-multiStratCsvSummarizer$summarize()
+# end region: execution of strategy
